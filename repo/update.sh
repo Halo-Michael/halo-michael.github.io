ccf4572ab9c0103ea155fc4b19efcd76fca18f1f
Halo-Michael <yc992147156@me.com>
2021-04-13 21:40:15 +0800

    Update

--- repo/update.sh
+++ repo/update.sh
@@ -4,10 +4,10 @@ for file in `ls update`
 do
 	if [[ ${file} == PPSSPP*ipa ]];then
 		mv update/${file} ../ipas
-		plutil -key items -key 0 -key assets -key 0 -key url -string "https://halo-michael.github.io/ipas/${file}" ../download/ppsspp_en_US.plist
-		plutil -key items -key 0 -key metadata -key title -string "$(echo ${file} | sed -e 's/.ipa//g')(Need AppSync)" ../download/ppsspp_en_US.plist
-		plutil -key items -key 0 -key assets -key 0 -key url -string "https://halo-michael.github.io/ipas/${file}" ../download/ppsspp_zh_CN.plist
-		plutil -key items -key 0 -key metadata -key title -string "$(echo ${file} | sed -e 's/.ipa//g')(需要 AppSync)" ../download/ppsspp_zh_CN.plist
+		plutil -replace items.0.assets.0.url -string "https://halo-michael.github.io/ipas/${file}" ../download/ppsspp_en_US.plist
+		plutil -replace items.0.metadata.title -string "$(echo ${file} | sed -e 's/.ipa//g')(Need AppSync)" ../download/ppsspp_en_US.plist
+		plutil -replace items.0.assets.0.url -string "https://halo-michael.github.io/ipas/${file}" ../download/ppsspp_zh_CN.plist
+		plutil -replace items.0.metadata.title -string "$(echo ${file} | sed -e 's/.ipa//g')(需要 AppSync)" ../download/ppsspp_zh_CN.plist
 	fi
 	if [[ ${file} == org.ppsspp.ppsspp*deb ]];then
 		mv update/${file} debs
